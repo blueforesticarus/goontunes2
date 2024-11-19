653e8422af64556576e538eb141238868c61ba2f
Erich Spaker <erichspaker@gmail.com>
2024-11-19 12:39:51 -0500

    noticable compile time improvement

--- devenv.nix
+++ devenv.nix
@@ -4,6 +4,7 @@
   # https://devenv.sh/basics/
   env.GREET = "(rust devenv)";
   env.LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+  env.RUSTC_WRAPPER = "${pkgs.sccache}/bin/sccache";
 
   languages.rust = {
     enable = true;
@@ -11,7 +12,7 @@
     channel = "nightly";
     mold.enable = true;
 
-    components = [ "rustc" "cargo" "clippy" "rustfmt" "rust-analyzer" ];
+    components = [ "rustc" "cargo" "clippy" "rustfmt" "rust-analyzer" "rustc-codegen-cranelift-preview"];
   };
 
   # https://devenv.sh/packages/
@@ -27,6 +28,8 @@
     cacert
     sqlite # for matrix_rust_sdk
     sqlite.dev
+
+    sccache
   ];
 
   enterShell = ''
